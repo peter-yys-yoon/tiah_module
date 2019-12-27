6fd6329092ea0af3daff6fe2086979d900c33ce7
peter-yys-yoon <peter.yys.yoon@gmail.com>
2019-12-27 14:36:09 +0900

    trim keyword excluded. video extention chagned mp4 >> avi

--- standalone/vtrim.py
+++ standalone/vtrim.py
@@ -27,10 +27,11 @@ def trim(opt):
 
     _path = video_path.replace('\\','/').split('/')
     # _vname = _path[-1]
-    _vname = _path[-1].split('.')[0]+'.mp4'
+    _vname = _path[-1].split('.')[0]+'.avi'
 
     if len(opt.outdir):
-        out_path = os.path.join(opt.outdir, '%s%s' % ('trim_', _vname))
+        # out_path = os.path.join(opt.outdir, '%s%s' % ('trim_', _vname))
+        out_path = os.path.join(opt.outdir, '%s' % (_vname))
     else:
         out_path = out_path = os.path.join('/'.join(_path[0:-1]), '%s%s' % ('trim_', _vname))
 
@@ -39,8 +40,8 @@ def trim(opt):
     props = get_properties(cap)
     fps = props['fps']
     s, e = range_count(opt.range, fps)
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-    # fourcc = cv2.VideoWriter_fourcc('H', '2', '6', '4')
+    # fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    fourcc = cv2.VideoWriter_fourcc('H', '2', '6', '4')
     # fourcc = VideoWriter_fourcc(*'MP4V')
 
     writer = cv2.VideoWriter(out_path, fourcc, props['fps'], (props['width'], props['height']))
@@ -68,9 +69,13 @@ def trim(opt):
 
 
 def range_count(raw, fps=30):
-    _s, _e = raw.split(',')
-    s = sec2count(min2sec(_s), fps=fps)
-    e = sec2count(min2sec(_e), fps=fps)
+
+    if raw == '-1':
+        return -1 ,99999999
+    else:
+        _s, _e = raw.split(',')
+        s = sec2count(min2sec(_s), fps=fps)
+        e = sec2count(min2sec(_e), fps=fps)
     return s, e
 
 
